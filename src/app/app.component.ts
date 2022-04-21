5dfb3524067224a6f36b6be646f36708858265bb
Eedulakanti Kumar Goud <ek21416@corp.ojas-it.com>
2022-04-21 10:52:36 +0530

    kumar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,47 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'application1';
-}
+
+  export class AppComponent {
+    title = 'Angular  CRUD operation with an Array Developed By kUMARGOUD';
+    employees = [
+      {name:"Amer", position:"Programmer"},
+      {name:"Durga", position:"Designer"},
+      {name:"Mani", position:"Manager"}
+    ];
+    model:any={};
+    model2:any={};
+    msg:any="";
+    addEmployee(){
+      this.employees.push(this.model);
+      this.model = {};
+      this.msg = "Record is successfully added..... "; 
+  
+    }
+    deleteEmployee(i:any){
+      this.employees.splice(i,1);
+      this.msg = "Record is successfully deleted..... ";
+      
+    }
+    myValue:any;
+    editEmployee(k:any){
+      this.model2.name = this.employees[k].name;
+      this.model2.position = this.employees[k].position;
+      this.myValue = k;
+  
+    }
+    updateEmployee(){
+      let k= this.myValue;
+      for(let i=0; i<this.employees.length;i++){
+        if(i==k){
+          this.employees[i]= this.model2;
+          this.model2 = {};
+          this.msg = "Record is successfully updated..... ";
+        }
+      }
+    }
+    clickMe(){
+      this.msg = "";
+    }
+  }
+
